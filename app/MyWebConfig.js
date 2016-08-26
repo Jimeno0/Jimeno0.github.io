177a3c9ae4ac033b3665798f57225a649364c109
Jimeno0 <carlospj@icloud.com>
2016-08-26 14:52:26 +0200

    most of the site sections content done,next step work on responsive and CSS

--- app/MyWebConfig.js
+++ app/MyWebConfig.js
@@ -2,4 +2,10 @@ angular
   .module('MyApp')
   .config(function($mdIconProvider, $mdThemingProvider){
     $mdIconProvider.icon('menu','svg/menu.svg',24);
+    $mdThemingProvider
+      .theme('default')
+      .primaryPalette('teal')
+    .accentPalette('light-blue')
+    .warnPalette('orange')
+    .backgroundPalette('grey');
   });
--- app/MyWebController.js
+++ app/MyWebController.js
@@ -4,8 +4,20 @@ angular
 
   function myWebControler ($http,$mdSidenav){
     var self=this;
+    var onScreen = 'img';
+
+    self.icons = [
+    {name:'github',     url:'svg/github-circle.svg',   href:'https://github.com/jimeno0' },
+    {name: 'linkedin',  url:'svg/linkedin.svg',        href:'https://www.linkedin.com/in/carlos-perez-jimeno-087b3390' },
+    {name:'twitter',    url:'svg/twitter.svg',         href:'https://twitter.com/jimeno0' },
+    {name: 'email',     url:'svg/email.svg',           href:'mailto:carlospj@icloud.com' }
+    ];
+
+
+
 
     self.toggleSidenav = toggleSidenav;
+    self.toggleMap = toggleMap;
 
     
     $http.get('data/section.json').then(function(response){
@@ -18,11 +30,28 @@ angular
 
     $http.get('data/portfolio.json').then(function(response){
           self.portfolio = response.data;
-          console.log(response.data);
+          
     });
 
     function toggleSidenav(){
       $mdSidenav('right').toggle();
     }
+
+
+    function toggleMap(){
+      var img = angular.element(document.getElementById('imgProfExp'));
+      var ifr = angular.element(document.getElementById('iframeProfExp'));
+      if (onScreen === 'img' ) {
+        img[0].style.display = 'none';
+        ifr[0].style.display = 'inline';
+        onScreen = 'ifr';
+      }
+      else {
+        img[0].style.display = 'inline';
+        ifr[0].style.display = 'none'; 
+        onScreen = 'img';
+      }
+
+    }    
 
   }
